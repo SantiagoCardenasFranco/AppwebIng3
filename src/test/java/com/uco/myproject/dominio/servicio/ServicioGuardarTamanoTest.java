75ce9a4c5ba890bb92587273f8be2752de965e99
MateoCR87 <mayteoxd66@gmail.com>
2022-03-05 00:58:51 -0500

    Extaclase 3.0

--- src/test/java/com/uco/myproject/dominio/servicio/ServicioGuardarTamanoTest.java
+++ src/test/java/com/uco/myproject/dominio/servicio/ServicioGuardarTamanoTest.java
@@ -15,7 +15,7 @@ import org.mockito.Mockito;
 class ServicioGuardarTamanoTest {
 
     @Test
-    void siNombreYaExisteDeberiaRetornarError() {
+    void siNombreExisteDeberiaRetornarError() {
 
         //arrange
         var tamano = new TamanoTestDataBuilder().build();
