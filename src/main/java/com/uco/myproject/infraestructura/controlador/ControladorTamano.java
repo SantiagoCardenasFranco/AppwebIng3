03847f167a2f189713631610b70e4efb0584fa22
MateoCR87 <mayteoxd66@gmail.com>
2022-03-05 01:19:24 -0500

    Extaclase 3.0

--- src/main/java/com/uco/myproject/infraestructura/controlador/ControladorTamano.java
+++ src/main/java/com/uco/myproject/infraestructura/controlador/ControladorTamano.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tamanos")
+@RequestMapping("/api/especificacion/tamanos")
 public class ControladorTamano {
 
     private final ServicioAplicacionGuardarTamano servicioAplicacionGuardarTamano;
