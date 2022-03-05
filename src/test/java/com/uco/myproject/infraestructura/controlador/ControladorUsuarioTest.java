2ed5d426dce8ff2c419161a08f5af7f0f35e95ab
MateoCR87 <mayteoxd66@gmail.com>
2022-03-05 12:15:17 -0500

    Extaclase 3.0 - Intento de solución

--- src/test/java/com/uco/myproject/infraestructura/controlador/ControladorUsuarioTest.java
+++ src/test/java/com/uco/myproject/infraestructura/controlador/ControladorUsuarioTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -38,7 +37,7 @@ class ControladorUsuarioTest {
     @Autowired
     private MockMvc mocMvc;
 
-    @Autowired
+    //@Autowired
     RepositorioUsuario repositorioUsuario;
 
     @Test
