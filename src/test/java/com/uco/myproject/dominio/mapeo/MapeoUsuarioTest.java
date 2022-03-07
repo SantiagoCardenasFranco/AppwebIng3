76c0854b15ac026f40afd74012ba2780aee11c34
MateoCR87 <mayteoxd66@gmail.com>
2022-03-06 21:10:02 -0500

    Extaclase 3.0 - Avances(Mapeo) y test

---
+++ src/test/java/com/uco/myproject/dominio/mapeo/MapeoUsuarioTest.java
@@ -0,0 +1,22 @@
+package com.uco.myproject.dominio.mapeo;
+
+import com.uco.myproject.aplicacion.dto.DtoUsuario;
+import com.uco.myproject.aplicacion.mapeo.UsuarioMaper;
+import com.uco.myproject.dominio.modelo.Usuario;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MapeoUsuarioTest {
+
+    public void mapeoTest(){
+        Usuario usuario = Usuario.of("Santiago", "Cárdenas Franco",
+                "hsdgahsdbjs", "skhdbcsjhb");
+
+        DtoUsuario dtoUsuario = UsuarioMaper.INSTANCIA.usuarioAUsuarioDto(usuario);
+
+        assertEquals("Santiago", dtoUsuario.getNombre());
+        assertEquals("Cárdenas Franco", dtoUsuario.getApellido());
+        assertEquals("hsdgahsdbjs", dtoUsuario.getCorreo());
+        assertEquals("skhdbcsjhb", dtoUsuario.getPassword());
+    }
+}
